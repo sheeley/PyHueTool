bfb27f2d41719b0b4765ccf9e8a78b2f8e1e982e
Johnny Sheeley <sheeley@aigee.org>
2014-09-24 13:21:36 -0700

    Simplify

--- hue.py
+++ hue.py
@@ -1,9 +1,11 @@
-from pyhue import Bridge
-from requests import get
+from requests import get, put
 from argparse import ArgumentParser
+from json import dumps
 
 URL = 'http://www.meethue.com/api/nupnp'
 USER_NAME = 'retailinnovation'
+ON_DICT = {"on": True, "bri": 255, "sat": 255, "hue": 0}
+OFF_DICT = {"on": False}
 
 
 def get_bridge_list():
@@ -11,14 +13,26 @@ def get_bridge_list():
     if response:
         return [bridge['internalipaddress'] for bridge in response.json()]
 
+
+def turn_group_off():
+    put('{}/groups/0/action'.format(BRIDGE_URL), dumps(OFF_DICT))
+
+
+def turn_light_on(light_id, light):
+    if light and not light.get('state', {}).get('reachable'):
+        print 'light {} does not appear to be reachable, skipping'.format(light_id)
+        return False
+    turn_group_off()
+    return put('{}/lights/{}/state'.format(BRIDGE_URL, light_id), dumps(ON_DICT))
+
 if __name__ == '__main__':
     parser = ArgumentParser('Hue tool to identify a specific light.')
-    parser.add_argument('light_id', help='Light to turn on, minimum id 1', type=int)
-    parser.add_argument('--ip', dest='ip', help='IP of hue bridge. Defaults to first bridge.')
-    parser.add_argument('--user', dest='user_name', default=USER_NAME, help='Authenticated user for hue.')
-    args, _ = parser.parse_known_args()
+    parser.add_argument('--light_id', help='Light to turn on, minimum id 1', type=int)
+    parser.add_argument('--ip', help='IP of hue bridge. Defaults to first bridge.')
+    parser.add_argument('--user', default=USER_NAME, help='Authenticated user for hue.')
+    args = parser.parse_args()
 
-    if args.light_id < 1:
+    if args.light_id is not None and args.light_id < 1:
         raise Exception('Hue light indices begin at 1.')
 
     if not args.ip:
@@ -28,12 +42,17 @@ if __name__ == '__main__':
     if not args.ip:
         raise Exception('Could not find hue to connect to.')
 
-    bridge = Bridge(args.ip, args.user_name)
+    BRIDGE_URL = 'http://{}/api/{}'.format(args.ip, args.user)
 
     print 'using bridge at: {}'.format(args.ip)
 
-    print 'turning all lights off'
-    bridge.groups[0].on = False
-
-    print 'setting light {} on'.format(args.light_id)
-    bridge.lights[args.light_id].on = True
+    if args.light_id:
+        resp = get('{}/lights/{}'.format(BRIDGE_URL, args.light_id))
+        light = resp.json()
+        turn_light_on(args.light_id, light)
+    else:
+        resp = get('{}/lights/'.format(BRIDGE_URL))
+        status = resp.json()
+        for light_id, light in status.iteritems():
+            if turn_light_on(light_id, light):
+                raw_input('light {} is on. Press enter for the next light.'.format(light_id))
